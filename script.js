5313fc0adb3e2c0cf3538c52c22998fa6fde4b72
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-29 22:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ function getFormvalue() {
 	let firstName = document.getElementById("fname").value.trim();
     let lastName = document.getElementById("lname").value.trim();
 	if (!firstName && !lastName) {
-		alert("Please enter your first and last name.");
-        return false;
-    }else if (!firstName) {
+        alert("Please enter your first and last name.");
+		return false;
+    }else if(!firstName) {
 		alert("Please enter your first name.");
         return false;
-    } else if (!lastName) {
+    }else if (!lastName){
 		alert("Please enter your last name.");
         return false;
     }
-	const fullName = `${firstName} ${lastName}`;
-	alert(fullName);
+	const fullName = `${firstName}${" "}${lastName}`;
+	if(fullName){
+		alert(fullName);
+	}else{
+		alert("Enter the full Name")
+	}
 	return false;
-	
-	
-
 }
